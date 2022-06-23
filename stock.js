4e7b507fd24392bd73cb092cbbc6476dae06cf38
Amorena Lucas <103912718+amorenalucas@users.noreply.github.com>
2022-06-22 23:57:05 -0300

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,7 @@
+let stockProductos = [
+    {id: 1, nombre: "Espejo irregular ", tipo: "espejo", desc: "Un espejo con onda", precio: 2500, espesor: "4 mm", img:'./img/espejo irregular.jpg'},
+    {id: 2, nombre: "Espejo redondo ", tipo: "espejo", desc: "Un espejo con onda", precio: 2300, espesor: "4 mm", img:'./img/espejo redondo.jpg'},
+    {id: 3, nombre: "Espejo redondo con luz ", tipo: "espejo", desc: "Un espejo con onda", precio: 15500, espesor: "4 mm", img:'./img/espejo redondo con luz.jpg'},
+    {id: 4, nombre: "Espejo con marco antiguo ", tipo: "espejo", desc: "Un espejo con onda", precio: 25000, espesor: "4 mm", img:'./img/espejo con marco antiguo.jpg'},
+
+]
